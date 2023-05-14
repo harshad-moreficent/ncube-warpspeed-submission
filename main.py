e9b04f41e3fef3f6928d11f37baf891f76f15206
Harshad Mac M1 <harshad@moreficent.com>
2023-05-14 08:56:13 +0530

    add character name to audio reply

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from typing import Dict
 import urllib
 
 from env_key import ENV_BOT_TOKEN, ENV_LOG_LEVEL, ENV_OPENAI_API_KEY, ENV_ELEVEN_LABS_API_KEY
-from model import Character
+from model import Character, ChatData
 
 log = logging.getLogger(Path(__file__).stem)
 
@@ -25,7 +25,7 @@ def get_audio(bot: telebot.TeleBot, chat_id: str, file_id: str):
     resp = requests.get(audio_url, allow_redirects=True)
     if resp is None:
         log.error(f'{chat_id} - Failed to get resp')
-    
+
     return resp.content
 
 
@@ -40,28 +40,41 @@ def run_bot(token: str, characters: Dict[str, Character]):
     character_names_md = '\n'.join(
         [f'{idx + 1}. **{x}**' for idx, x in enumerate(character_names)])
 
+    state: Dict[int, ChatData] = {}
+
     def handle_message(message: Message):
         chat_id = message.chat.id
+
+        chat_data = state.get(chat_id)
+        if chat_data is None:
+            log.error(f'{chat_id} - chat data absent')
+            bot.send_message(chat_id,
+                             'Something has gone wrong. Please clear chat')
+            return
+
         content_type = message.content_type
-        print(message.content_type)
         sent_message = None
         if content_type == 'text':
-            log.debug(f'{chat_id} - Got text input')
+            log.info(f'{chat_id} - Got text input')
             sent_message = bot.send_message(chat_id, message.text)
         elif content_type == 'voice':
-            log.debug(f'{chat_id} - got voice input')
+            log.info(f'{chat_id} - got voice input')
             audio_data = get_audio(bot, chat_id, message.voice.file_id)
             if audio_data is None:
                 log.error('{chat_id} - Failed to get audio file')
                 sent_message = bot.send_message(
                     chat_id, 'Something went wrong. Please retry.')
             else:
-                sent_message = bot.send_audio(chat_id, audio_data)
+                sent_message = bot.send_audio(chat_id,
+                                              audio_data,
+                                              reply_to_message_id=message.id,
+                                              performer=chat_data.character_name)
         else:
             log.error(
                 f'{chat_id} - Got unhandled content type: {content_type}')
             sent_message = bot.send_message(
                 chat_id, "Your reply must be either text or a voice note")
+
         bot.register_next_step_handler(sent_message, handle_message)
 
     def chat_init_handler(message: Message):
@@ -80,13 +93,14 @@ def run_bot(token: str, characters: Dict[str, Character]):
             )
             bot.send_message(chat_id, "Setting things up...")
             # set up chat llm and other APIs
+            state.update({chat_id: ChatData(character_name=character_name)})
             sent_message = bot.send_message(
                 chat_id, f'Done. You are now chatting with {character_name}')
             bot.register_next_step_handler(sent_message, handle_message)
 
     @bot.message_handler(commands=["start"])
     def start_handler(message: Message):
-        log.debug('New Conversation')
+        log.info('New Conversation')
         sent_message = bot.reply_to(
             message,
             f'Hi, welcome to {BOT_NAME}. Please choose the celebrity you wish to chat with:\n\n{character_names_md}',
@@ -122,7 +136,7 @@ def main():
     if openai_api_key is None:
         log.error(f'{ENV_OPENAI_API_KEY} not set')
         return
-    
+
     eleven_labs_api_key = os.getenv(ENV_ELEVEN_LABS_API_KEY)
     if eleven_labs_api_key is None:
         log.error(f'{ENV_ELEVEN_LABS_API_KEY} not set')
--- model.py
+++ model.py
@@ -8,3 +8,8 @@ class Character:
     voice: str
     tts_model: str
     sort_order: int
+
+
+@dataclass
+class ChatData:
+    character_name: str
